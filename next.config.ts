c45b9eebae9d9b39e43f9cc4e7b0da5d3861ac4b
Aaditi <aaditi04vithal@gmail.com>
2026-04-12 16:19:50 +0530

    Fix Prisma setup with DB

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
+  serverExternalPackages: ["@prisma/client", "prisma"],
   /* config options here */
   images: {
     remotePatterns: [
